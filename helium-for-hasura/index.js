ba13c83ae8482c52a201199ce491e152fdee1790
Akshaya Acharya <akshaya.acharya.01@gmail.com>
2023-04-28 02:45:23 +0530

    Use hfh to add relationships and authorisation to sample app, add admin secret to the hasura server

--- helium-for-hasura/index.js
+++ helium-for-hasura/index.js
@@ -1,1 +1,18 @@
-// for every model in the models folder, compile the model into a thing that tracks the table
+const modelExports = ({
+  belongsTo,
+  hasOne,
+  hasMany,
+  permission,
+  exists,
+  and,
+  or,
+  not,
+  eq,
+  ne,
+  gt,
+  lt,
+  gte,
+  lte,
+} = require("./model"));
+
+module.exports = modelExports;
